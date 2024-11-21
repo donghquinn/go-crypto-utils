7bd8ebc10345234f3b63322f43901f751c1c87d9
dong hyun kim <ehdgus1524@gmail.com>
2024-11-21 16:05:06 +0900

    test code

--- test/aes.test.go
+++ test/aes.test.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"testing"
 
@@ -13,9 +12,9 @@ type testInterface struct {
 	Email string `json:"email"`
 }
 
-func testAes(_ *testing.T) {
+func TestAes(t *testing.T) {
 	newKey, _ := biz.GenerateRandomAES256Key(32)
-	_, hexKey, base64Key := biz.GenKey(newKey)
+	hexKey, base64Key := biz.GenKey(newKey)
 
 	log.Printf("newKey: %v", string(base64Key))
 
@@ -24,27 +23,30 @@ func testAes(_ *testing.T) {
 	// 구조체를 JSON으로 직렬화
 	plaintextBytes, err := json.Marshal(text)
 	if err != nil {
-		fmt.Println("구조체 직렬화 오류:", err)
+		t.Fatal("구조체 직렬화 오류:", err)
 		return
 	}
 
 	encryptedString, encryptErr := biz.EncryptAES256CBC(plaintextBytes, newKey)
 
 	if encryptErr != nil {
-		fmt.Println("암호화 오류:", encryptErr)
+		t.Fatal("암호화 오류:", encryptErr)
 		return
 	}
 
 	decryptedString, decryptErr := biz.DecryptAES256CBC(encryptedString, newKey)
 
 	if decryptErr != nil {
-		fmt.Println("복호화 오류:", decryptErr)
+		t.Fatal("복호화 오류:", decryptErr)
 		return
 	}
 
-	fmt.Println("암호키 (Hex):", hexKey)
-	fmt.Println("암호키 (Base64):", base64Key)
-	fmt.Println("암호화된 문자: ", string(encryptedString))
-	fmt.Println("복호화된 평문:", string(decryptedString))
+	t.Log("암호키 (Hex):", hexKey)
+	t.Log("암호키 (Base64):", base64Key)
+	t.Log("암호화된 문자: ", string(encryptedString))
+	t.Log("복호화된 평문:", string(decryptedString))
 
+	if string(plaintextBytes) != string(decryptedString) {
+		t.Errorf("Decrypted string is not equal to original string")
+	}
 }
